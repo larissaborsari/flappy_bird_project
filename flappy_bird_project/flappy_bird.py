42a4a525eafdbc6f1caad387a171eeb6cef9054e
Lari <larissaborsari@hotmail.com>
2022-09-23 15:00:04 -0300

    last commit

--- flappy_bird_project/flappy_bird.py
+++ flappy_bird_project/flappy_bird.py
@@ -16,3 +16,37 @@ IMAGENS_PASSARO = [
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird2.png'))),
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird3.png'))),
 ]
+
+pygame.font.init()
+FONTE_PONTOS = pygame.font.SysFont('arial', 50)
+
+##Criando objetos do jogo (bird, pipe, background and base)
+
+class Passaro:
+    IMGS = IMAGENS_PASSARO
+
+    #animações da rotação/inclinação
+    ROTACAO_MAXIMA = 25
+    VELOCIDADE_ROTACAO = 20
+    TEMPO_ANIMACAO = 5
+
+    #função que cria o pássaro
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.angulo = 0
+        self.velocidade = 0
+        self.altura = self.y
+        self.tempo = 0
+        self.contagem_imagem = 0
+        self.imagem = IMGS[0]
+
+
+class Cano:
+    pass
+
+class Chao:
+    pass
+
+
+
