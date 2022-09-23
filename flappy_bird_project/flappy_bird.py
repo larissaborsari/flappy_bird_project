bdc8faed2c6e3c47838395d259325d3874ab2331
Lari <larissaborsari@hotmail.com>
2022-09-23 14:45:09 -0300

    commit codigo jogo

---
+++ flappy_bird_project/flappy_bird.py
@@ -0,0 +1,18 @@
+#Recriando o jogo
+
+##Importações necessárias
+import pygame
+import os
+import random
+
+TELA_LARGURA = 500
+TELA_ALTURA = 800
+
+IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','pipe.png')))
+IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','base.png')))
+IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bg.png')))
+IMAGENS_PASSARO = [
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird3.png'))),
+]
