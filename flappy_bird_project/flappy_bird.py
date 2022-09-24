f5d7b21f07f43b39b704ffde6fa822118d2cf5fe
Lari <larissaborsari@hotmail.com>
2022-09-24 10:10:04 -0300

    last commit

--- flappy_bird_project/flappy_bird.py
+++ flappy_bird_project/flappy_bird.py
@@ -136,12 +136,19 @@ class Cano:
 
         distancia_topo = (self.x - passaro.x, self.pos_topo - round(passaro.y))
         distancia_base = (self.x - passaro.x, self.pos_base - round(passaro.y))
-        
 
+        topo_ponto = passaro_mask.overlap(topo_mask, distancia_topo)
+        base_ponto = passaro_mask.overlap(base_mask, distancia_base)
+
+        if base_ponto or topo_ponto: #se algum dos pontos de colisão é verdadeiro
+            return True
+        else:
+            return False
 
 
 class Chao:
-    pass
+    VELOCIDADE = 5
+    LARGURA = 
 
 
 
